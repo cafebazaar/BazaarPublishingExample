b8597aee4bd6891a788a11be5617af7d5572cf41
Mansour <mansurjavadi@gmail.com>
2021-12-08 16:48:48 +0330

    Add infra for superSDK

---
+++ SuperSDK Wizard/Assets/Cafebazaar/SuperSDK/BazaarSuperSDK.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BazaarSuperSDK : MonoBehaviour
+{
+    [HideInInspector]
+    public string version = "1.0.0";
+    public string tapsellToken = "";
+    public string tapsellZoneId = "";
+    public string appMettricaApiKey = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
